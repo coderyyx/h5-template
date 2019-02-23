947c04e6fba66c2b6bb7c3455dd9b0d0d296bdae
coderyyx <yyxcoder@163.com>
2019-02-23 21:39:48 +0800

    modify info.js

--- src/utils/info.js
+++ src/utils/info.js
@@ -1,5 +1,6 @@
 const config = {
-    appId : 'wx1c1fa5d121654c89', 
+    // appId : 'wx1c1fa5d121654c89',
+    appId: 'wx36b5e8c4553dabb9', 
     hostname : 'm.besthinghr.com', 
     apiurl : 'http://mob.besthinghr.com/',
     testapiurl : 'http://mob.besthinghr.com/'
